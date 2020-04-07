eb00c4eb3f8473d56617d5867f2bc38cde7bd085
gifgif1995 <ryan.giffin1000@gmail.com>
2020-04-07 13:15:19 -0600

    First Commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing {
+    class Program {
+        static void Main (string[] args) {
+
+            List<object> boxList = new List<object> ();
+            boxList.Add (7);
+            boxList.Add (28);
+            boxList.Add (-1);
+            boxList.Add (true);
+            boxList.Add ("chair");
+
+            int sum = 0;
+
+            foreach (var item in boxList) {
+                if (item is int) {
+                    int IsInt = (int) item;
+                    sum += IsInt;
+                }
+                Console.WriteLine (item);
+            }
+            Console.WriteLine ("Sum: " + sum);
+        }
+    }
+}
